a3eb29172183c3b6f43e747c71a80f7ec260f2a2
Joren Van Hecke <joren.vanhecke@student.kuleuven.be>
2022-05-16 13:13:40 +0200

    Refactoring performance benchmarking
    * Move old test class files to separate folder
    * Class MyBenchmark.kt updated to version of previous benchmark
    * New benchmark test lambda's

--- test/src/main/kotlin/org/sample/MyBenchmark.kt
+++ test/src/main/kotlin/org/sample/MyBenchmark.kt
@@ -1,48 +1,56 @@
-/*
- * Copyright (c) 2014, Oracle America, Inc.
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met:
- *
- *  * Redistributions of source code must retain the above copyright notice,
- *    this list of conditions and the following disclaimer.
- *
- *  * Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in the
- *    documentation and/or other materials provided with the distribution.
- *
- *  * Neither the name of Oracle nor the names of its contributors may be used
- *    to endorse or promote products derived from this software without
- *    specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
- * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
- * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
- * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
- * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
- * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
- * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
- * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
- * THE POSSIBILITY OF SUCH DAMAGE.
- */
-
 package org.sample
 
 import org.openjdk.jmh.annotations.Benchmark
+import test.microbenchmarking.Main
 
 open class MyBenchmark {
 
     @Benchmark
-    fun testMethod(): Int {
-        var j = 0
-        for (i in 1..3000000) {
-            val lambda = { a: Int -> a * a }
-            j += lambda(i)
-        }
-        return j
+    fun emptyClosure3MillionCalls(): Int {
+        val main = Main()
+        return main.emptyClosure3MillionCalls()
+    }
+
+    @Benchmark
+    fun emptyClosure3MillionAssignmentsAndCalls(): Int {
+        val main = Main()
+        return main.emptyClosure3MillionAssignmentsAndCalls()
+    }
+
+    @Benchmark
+    fun multipleEmptyClosure3MillionCalls(): Int {
+        val main = Main()
+        return main.multipleEmptyClosure3MillionCalls()
+    }
+
+    @Benchmark
+    fun multipleEmptyClosure3MillionAssignmentsAndCalls(): Int {
+        val main = Main()
+        return main.multipleEmptyClosure3MillionAssignmentsAndCalls()
+    }
+
+    @Benchmark
+    fun nonEmptyClosure3MillionCalls(): Int {
+        val main = Main()
+        return main.nonEmptyClosure3MillionCalls()
+    }
+
+    @Benchmark
+    fun nonEmptyClosure3MillionAssignmentsAndCalls(): Int {
+        val main = Main()
+        return main.nonEmptyClosure3MillionAssignmentsAndCalls()
+    }
+
+    @Benchmark
+    fun multipleNonEmptyClosure3MillionCalls(): Int {
+        val main = Main()
+        return main.multipleNonEmptyClosure3MillionCalls()
+    }
+
+    @Benchmark
+    fun multipleNonEmptyClosure3MillionAssignmentsAndCalls(): Int {
+        val main = Main()
+        return main.multipleNonEmptyClosure3MillionAssignmentsAndCalls()
     }
 
 }
