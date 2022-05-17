fd21b0ec18e244e042dfdd95d57357e20af19498
Joren Van Hecke <joren.vanhecke@student.kuleuven.be>
2022-05-17 16:57:38 +0200

    Install scripts for project 'test' fixed
    * The scripts installed the library that was
    produced after lambda merging, instead of the
    original library

--- test/install-test-classes.bat
+++ test/install-test-classes.bat
@@ -1,1 +1,1 @@
-mvn install:install-file -Dfile="..\lambda-test-classes\build\proguard\test-classes-lg-2.1.0.jar" -DgroupId="test.microbenchmarking" -DartifactId="benchmarking-lg" -Dversion="2.1.0" -Dpackaging="jar" -DgeneratePom=true -DcreateChecksum=true
+mvn install:install-file -Dfile="..\lambda-test-classes\build\libs\test-classes-2.1.0.jar" -DgroupId="test.microbenchmarking" -DartifactId="benchmarking" -Dversion="2.1.0" -Dpackaging="jar" -DgeneratePom=true -DcreateChecksum=true
