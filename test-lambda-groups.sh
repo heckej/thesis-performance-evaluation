3764f296b5468680e299257063678f8be79c23e2
Joren Van Hecke <joren.vanhecke@student.kuleuven.be>
2022-05-18 00:12:41 +0200

    Test script lambda groups corrected + common script

--- test-lambda-groups.sh
+++ test-lambda-groups.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/sh
-cd test
+cd test-lambda-groups
 cd ubuntu
-java -jar target/benchmarks.jar -f5 -i5 -rff test-instance-1.4.8.11.15.19.23.csv -prof jfr  closure1FreeVariables3MillionInstantiations closure4FreeVariables3MillionInstantiations closure8FreeVariables3MillionInstantiations closure11FreeVariables3MillionInstantiations closure15FreeVariables3MillionInstantiations closure19FreeVariables3MillionInstantiations closure23FreeVariables3MillionInstantiations > test-instance-1.4.8.11.15.19.23.log
+java -jar ../target/benchmarks.jar -f5 -i5 -rff test-instance-1.4.8.11.15.19.23.csv -prof jfr  closure1FreeVariables3MillionInstantiations closure4FreeVariables3MillionInstantiations closure8FreeVariables3MillionInstantiations closure11FreeVariables3MillionInstantiations closure15FreeVariables3MillionInstantiations closure19FreeVariables3MillionInstantiations closure23FreeVariables3MillionInstantiations > test-instance-1.4.8.11.15.19.23.log
