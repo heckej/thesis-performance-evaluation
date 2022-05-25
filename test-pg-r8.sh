b46fcf4681b7423018940f35bd4fe2d4ce89b43e
Joren Van Hecke <joren.vanhecke@student.kuleuven.be>
2022-05-25 10:28:57 +0200

    Script to run benchmark for both pg and R8

---
+++ test-pg-r8.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/sh
+cd test-pg-lambda-groups
+cd ubuntu
+java -jar ../target/benchmarks.jar -f5 -i5 -rff test-closure-instance-call-arity-call-1.2.4.8.9.11.15.16.19.23.csv  > test-closure-instance-call-arity-call-1.2.4.8.9.11.15.16.19.23.log
+cd test-r8
+cd ubuntu
+java -jar ../target/benchmarks.jar -f5 -i5 -rff test-closure-instance-call-arity-call-1.2.4.8.9.11.15.16.19.23.csv  > test-closure-instance-call-arity-call-1.2.4.8.9.11.15.16.19.23.log
