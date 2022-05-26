01a0de0f62866506058b9c71025556e6578be79e
Joren Van Hecke <joren.vanhecke@student.kuleuven.be>
2022-05-26 11:41:12 +0200

    Update library version in pom.xml and install scripts

--- test-pg-lambda-groups/install-test-classes.sh
+++ test-pg-lambda-groups/install-test-classes.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/sh
-mvn install:install-file -Dfile="../lambda-test-classes/build/proguard/test-classes-pg-2.1.0.jar" -DgroupId="test.microbenchmarking" -DartifactId="benchmarking-pg" -Dversion="2.1.0" -Dpackaging="jar" -DgeneratePom=true -DcreateChecksum=true
+mvn install:install-file -Dfile="../lambda-test-classes/build/proguard/test-classes-pg-2.2.0.jar" -DgroupId="test.microbenchmarking" -DartifactId="benchmarking-pg" -Dversion="2.2.0" -Dpackaging="jar" -DgeneratePom=true -DcreateChecksum=true
 
--- test-r8/install-test-classes.bat
+++ test-r8/install-test-classes.bat
@@ -1,1 +1,1 @@
-mvn install:install-file -Dfile="..\lambda-test-classes\build\r8\test-classes-r8-2.1.0.jar" -DgroupId="test.microbenchmarking" -DartifactId="benchmarking-r8" -Dversion="2.1.0" -Dpackaging="jar" -DgeneratePom=true -DcreateChecksum=true
+mvn install:install-file -Dfile="..\lambda-test-classes\build\r8\test-classes-r8-2.2.0.jar" -DgroupId="test.microbenchmarking" -DartifactId="benchmarking-r8" -Dversion="2.2.0" -Dpackaging="jar" -DgeneratePom=true -DcreateChecksum=true
