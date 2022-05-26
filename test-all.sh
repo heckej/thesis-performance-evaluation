3cacbc7166419cb7adf87e91378fdeb0e373ec52
Joren Van Hecke <joren.vanhecke@student.kuleuven.be>
2022-05-26 15:48:05 +0200

    Benchmark scripts prepared for arity calls

--- test-all.sh
+++ test-all.sh
@@ -2,12 +2,16 @@
 ./test.sh;
 git add .
 git commit -m "Benchmark results without transformations"
-#./test-lambda-groups.sh;
-#git add .
-#git commit -m "Benchmark results ProGuard with lambda merging"
-#./test-pg.sh;
-#git add .
-#git commit -m "Benchmark results ProGuard with shrinkig, optimisation and lambda merging"
+git push
+./test-lambda-groups.sh;
+git add .
+git commit -m "Benchmark results ProGuard with lambda merging"
+git push
+./test-pg.sh;
+git add .
+git commit -m "Benchmark results ProGuard with shrinkig, optimisation and lambda merging"
+git push
 ./test-r8.sh
 git add .
 git commit -m "Benchmark results R8 with shrinking and optimisation"
+git push
--- test-lambda-groups.sh
+++ test-lambda-groups.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/sh
 cd test-lambda-groups
 cd ubuntu
-java -jar ../target/benchmarks.jar -f5 -i5 -rff test-closure_instance-0-0.7.10.14.18.21.csv -e FreeVariables3MillionInstantiationsAndCalls closure7FreeVariables3MillionInstantiations closure14FreeVariables3MillionInstantiations closure18FreeVariables3MillionInstantiations closure0FreeVariables3MillionInstantiations closure10FreeVariables3MillionInstantiations closure21FreeVariables3MillionInstantiations > test-closure_instance-0-0.7.10.14.18.21.log
+java -jar ../target/benchmarks.jar -f5 -i5 -rff test-arity-call-1.2.4.8.9.11.15.16.19.23.csv -e "Instantiations|Free" e2F e1F 11 13 15 16 19 23 e1A e2A e4A e6A e8A e9A e4F e6F e8F e9F  > test-arity-call-1.2.4.8.9.11.15.16.19.23.log
