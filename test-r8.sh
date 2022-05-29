7beb9d71639ce346deff177dabc79c8866c5c2dc
Joren Van Hecke <joren.vanhecke@student.kuleuven.be>
2022-05-29 14:13:02 +0200

    Scripts to benchmark calls of all closure sizes, except for 0

--- test-r8.sh
+++ test-r8.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/sh
 cd test-r8
 cd ubuntu
-java -jar ../target/benchmarks.jar -f5 -i5 -rff test-arity-call-all.csv -e "Instantiations|Free|e0A|e0F" > test-arity-call-all.log
+java -jar ../target/benchmarks.jar -f5 -i5 -rff test-closure-call-all.csv -e "Instantiations|Argument|e0A|e0F" > test-closure-call-all.log
