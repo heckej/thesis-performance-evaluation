8ac0c3de63966b0415d57f0853af6ed5ec384cae
Joren Van Hecke <joren.vanhecke@student.kuleuven.be>
2022-05-29 14:21:57 +0200

    Updated commit messages for closure call benchmark tests

--- test-all.sh
+++ test-all.sh
@@ -1,17 +1,17 @@
 #!/usr/bin/sh
 ./test.sh;
 git add .
-git commit -m "Benchmark results without transformations: arity/closure call other arities/cosure sizes"
+git commit -m "Benchmark results without transformations: closure call non-zero closure sizes"
 git push
 ./test-lambda-groups.sh;
 git add .
-git commit -m "Benchmark results ProGuard with lambda merging arity/closure call other arities/cosure sizes"
+git commit -m "Benchmark results ProGuard with lambda merging: closure call non-zero closure sizes"
 git push
 ./test-pg.sh;
 git add .
-git commit -m "Benchmark results ProGuard with shrinking, optimisation and lambda merging arity/closure call other arities/cosure sizes"
+git commit -m "Benchmark results ProGuard with shrinking, optimisation and lambda merging: closure call non-zero closure sizes"
 git push
 ./test-r8.sh
 git add .
-git commit -m "Benchmark results R8 with shrinking and optimisation arity/closure call other arities/cosure sizes"
+git commit -m "Benchmark results R8 with shrinking and optimisation: closure call non-zero closure sizes"
 git push
