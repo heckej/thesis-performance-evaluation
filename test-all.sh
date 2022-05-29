60644e7375a8a54de5263a43d88748c093dafade
Joren Van Hecke <joren.vanhecke@student.kuleuven.be>
2022-05-30 00:21:12 +0200

    Benchmark results R8 with shrinking and optimisation: closure call non-zero closure sizes

--- test-all.sh
+++ test-all.sh
@@ -1,16 +1,17 @@
+cd /mnt/back-up/Laptop-Joren/d/Master-CS-projects/masterproef/performance-evaluation
 #!/usr/bin/sh
-./test.sh;
-git add .
-git commit -m "Benchmark results without transformations: closure call non-zero closure sizes"
-git push
-./test-lambda-groups.sh;
-git add .
-git commit -m "Benchmark results ProGuard with lambda merging: closure call non-zero closure sizes"
-git push
-./test-pg.sh;
-git add .
-git commit -m "Benchmark results ProGuard with shrinking, optimisation and lambda merging: closure call non-zero closure sizes"
-git push
+#./test.sh;
+#git add .
+#git commit -m "Benchmark results without transformations: closure call non-zero closure sizes"
+#git push
+#./test-lambda-groups.sh;
+#git add .
+#git commit -m "Benchmark results ProGuard with lambda merging: closure call non-zero closure sizes"
+#git push
+#./test-pg.sh;
+#git add .
+#git commit -m "Benchmark results ProGuard with shrinking, optimisation and lambda merging: closure call non-zero closure sizes"
+#git push
 ./test-r8.sh
 git add .
 git commit -m "Benchmark results R8 with shrinking and optimisation: closure call non-zero closure sizes"
