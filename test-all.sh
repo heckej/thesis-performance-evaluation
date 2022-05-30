98ed37c6d9934fe8f9959f03872887ba7353ed91
Joren Van Hecke <joren.vanhecke@student.kuleuven.be>
2022-05-30 22:54:52 +0200

    Prepare to only run r8 benchmark next time

--- test-all.sh
+++ test-all.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/sh
 cd /mnt/back-up/Laptop-Joren/d/Master-CS-projects/masterproef/performance-evaluation
-./test.sh;
-./test-lambda-groups.sh;
-./test-pg.sh;
-/mnt/back-up/Laptop-Joren/d/Master-CS-projects/masterproef/performance-evaluation/test-r8.sh
+#./test.sh;
+#./test-lambda-groups.sh;
+#./test-pg.sh;
+./test-r8.sh
