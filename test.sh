8444fc4723f6f1c7c67220ac7ef0658f9f770f0b
Joren Van Hecke <joren.vanhecke@student.kuleuven.be>
2022-06-06 23:32:43 +0200

    test.sh script for profiler

--- test.sh
+++ test.sh
@@ -3,7 +3,7 @@ cd test
 cd ubuntu
 # for csv: -rff test-closure-instance-all.csv
 # -e "Call|Argument|e0A|e0F"
-java -jar ../target/benchmarks.jar -f5 -i5 -prof jfr   > test-closure-1-2-call-profile.log
+java -jar ../target/benchmarks.jar -f5 -i5 -prof jfr test.MyBenchmark.closure1FreeVariables3MillionCalls test.MyBenchmark.closure2FreeVariables3MillionCalls > test-closure-1-2-call-profile.log
 git add .
 git commit -m "Benchmark profile results without transformations: closure 1, 2 call"
 ../push.git.sh
